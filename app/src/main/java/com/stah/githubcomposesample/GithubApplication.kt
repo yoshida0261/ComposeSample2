8c5fa91589df68c9f72620078fe5b59642eae852
yoshida0261 <yoshida261@gmail.com>
2021-09-21 01:20:53 +0900

    applicationクラス 追加

--- app/src/main/java/com/stah/githubcomposesample/GithubApplication.kt
+++ app/src/main/java/com/stah/githubcomposesample/GithubApplication.kt
@@ -1,4 +1,8 @@
 package com.stah.githubcomposesample
 
-class GithubApplication {
-}
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class GithubApplication : Application()
