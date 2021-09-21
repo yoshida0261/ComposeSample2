9ce4d954edcb197f12f04e041de94b7337145c94
yoshida0261 <yoshida261@gmail.com>
2021-09-22 04:49:19 +0900

    add viewModel

---
+++ app/src/main/java/com/stah/githubcomposesample/viewmodel/MainViewModel.kt
@@ -0,0 +1,43 @@
+package com.stah.githubcomposesample.viewmodel
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.stah.githubcomposesample.model.repository.User
+import com.stah.githubcomposesample.model.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val userRepository: UserRepository
+) : ViewModel() {
+
+    sealed class UiState {
+        object Initial : UiState()
+        object Loading : UiState()
+        data class Success(val user: User) : UiState()
+        object Failure : UiState()
+    }
+
+    val uiState: MutableState<UiState> = mutableStateOf(UiState.Initial)
+
+    val searchQuery: MutableState<String> = mutableStateOf("")
+
+    fun onSearchTapped() {
+        val searchQuery: String = searchQuery.value
+
+        viewModelScope.launch {
+            uiState.value = UiState.Loading
+            kotlin.runCatching {
+                userRepository.getUser(userName = searchQuery)
+            }.onSuccess {
+                uiState.value = UiState.Success(user = it)
+            }.onFailure {
+                uiState.value = UiState.Failure
+            }
+        }
+    }
+}
