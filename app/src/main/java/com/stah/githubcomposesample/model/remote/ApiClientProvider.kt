7d73f659a3d07e1794f7a1073b480b857f29aff2
yoshida0261 <yoshida261@gmail.com>
2021-09-22 05:38:36 +0900

    okhttp3のloggingを追加

--- app/src/main/java/com/stah/githubcomposesample/model/remote/ApiClientProvider.kt
+++ app/src/main/java/com/stah/githubcomposesample/model/remote/ApiClientProvider.kt
@@ -4,8 +4,14 @@ import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFact
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import javax.inject.Inject
+import okhttp3.OkHttpClient
+
+import okhttp3.Interceptor
+import okhttp3.Request
+
 
 class ApiClientProvider @Inject constructor() {
     companion object {
@@ -14,6 +20,25 @@ class ApiClientProvider @Inject constructor() {
 
     @ExperimentalSerializationApi
     fun provide(): ApiClient {
+
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(Interceptor { chain ->
+            val original: Request = chain.request()
+
+            //header設定
+            val request: Request = original.newBuilder()
+                .header("Accept", "application/json")
+                .method(original.method, original.body)
+                .build()
+            chain.proceed(request)
+        })
+        //ログ出力設定
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BASIC)
+        httpClient.addInterceptor(logging)
+        val client: OkHttpClient = httpClient.build()
+
+
         return Retrofit.Builder()
             .baseUrl(API_END_POINT)
             .addConverterFactory(
@@ -23,6 +48,7 @@ class ApiClientProvider @Inject constructor() {
                     "application/json".toMediaType()
                 ),
             )
+            .client(client)
             .build()
             .create(ApiClient::class.java)
     }
